0777e337713c5809e2887864d2cd09d940659ada
Eulogia_Koinē <elruien0604@gmail.com>
2023-07-26 11:12:47 +0900

    extension 1.2.1 Math.combination 오류 수정
    Cf, Pf 감마함수 참조 오타 수정

--- extension/Math/combination/Cf.js
+++ extension/Math/combination/Cf.js
@@ -1,6 +1,6 @@
 "use strict"
 module.exports = function(path, r_inspire){
-const gamme = r_inspire('gamma').gamma
+const gamma = r_inspire('gamma').gamma
 function Cf(n, r){
     if(r < 0 || r > n) return 0
     return gamma(n+1) / gamma(r+1) / gamma(n-r+1)
--- extension/Math/combination/Pf.js
+++ extension/Math/combination/Pf.js
@@ -3,7 +3,7 @@ module.exports = function(path, r_inspire){
 const gamma = r_inspire('gamma').gamma
 function Pf(n, r){
     if(r < 0 || r > n) return 0
-    return g(n+1) / g(n-r+1)
+    return gamma(n+1) / gamma(n-r+1)
 }
 if(!Math.Pf) Math.Pf = Pf
 return { Pf: Pf }
